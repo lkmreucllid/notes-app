af92fc676868d2f215eeee8b9d0e4ca3b3c12a7b
lkmreucllid <lkmreucllid01@gmail.com>
2022-04-05 16:47:07 +0530

    customizing yargs

--- app.js
+++ app.js
@@ -2,13 +2,44 @@ const chalk = require('chalk')
 const yargs = require('yargs')
 const addNotes = require('./notes.js')
 
-const command = process.argv[2]
+//Customizing yargs
+yargs.version('1.1.0')
 
-if(command === 'add'){
-    console.log('Adding Notes')
-}else if(command === 'remove'){
-    console.log('Removing Notes!...')
-    console.log(process.argv[3])
-}
+//add, remove, read, list all the notes
+
+//Create Add
+yargs.command({
+    command: 'add',
+    describe:'Add new Note',
+    handler:function (){
+        console.log('Adding a new Note!')
+    }
+
+})
+
+//Create Remove
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function(){
+        console.log('Removing a Note!')
+    }
+})
+
+//Create a Read
+yargs.command({
+    command:'read',
+    describe:'show a single note by id',
+    handler:function(){
+        console.log('Showing a single note by id provided')
+    }
+})
+
+//Create List all Notes
+yargs.command({
+    command: 'list',
+    describe:'List all the notes',
+    handler:function(){console.log('Time to List all the notes!')}
+})
 
 console.log(yargs.argv)
